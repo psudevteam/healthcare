62fe9655ad90cf77fc04ae6248d3e674cfa13d03
Fenny Oktaviani <fnny04@gmail.com>
2022-05-18 15:59:10 +0700

    Fenny Oktaviani | add new db

--- healthcare.sql
+++ healthcare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: healthcare-db
--- Generation Time: May 16, 2022 at 03:02 PM
+-- Generation Time: May 18, 2022 at 08:57 AM
 -- Server version: 8.0.29
 -- PHP Version: 8.0.19
 
@@ -41,7 +41,31 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `price`, `desc`, `pic`, `qty`) VALUES
-(1, 'Paracetamol', '7000', 'Obat pereda Demam', 'https://static.hdmall.id/750x450/system/image_attachments/images/000/011/744/original/paracetamol-if-500mg-tab-str-1.jpg', 100);
+(1, 'Paracetamol', '7000', 'Obat pereda Demam', 'https://static.hdmall.id/750x450/system/image_attachments/images/000/011/744/original/paracetamol-if-500mg-tab-str-1.jpg', 100),
+(2, 'Amoxilin', '6000', 'OBat jengjeriheun', 'https://images.k24klik.com/product/large/apotek_online_k24klik_20211130101224359225_AMOXICILLIN-PHARMALAB-500MG-TAB-100S.jpg', 80),
+(3, 'Upixon', '16000', 'Obat Cacing', 'https://images.tokopedia.net/img/cache/700/product-1/2020/7/4/100550299/100550299_06b6e479-9308-4b09-b6ec-39299a521512_786_786.jpg', 75),
+(4, 'Promag', '15000', 'Obat asam lambung', 'https://image-cdn.medkomtek.com/ABfGhUV5kWlx5YWP4eQUvnNHLLs=/673x373/smart/klikdokter-media-buckets/medias/2311507/original/007215200_1578449437-Promag_12Tablet_1box_2.jpg', 75);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `role` int NOT NULL DEFAULT '1',
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `role`, `email`, `password`) VALUES
+(1, 'Mpun', 2, 'fnny04@gmail.com', 'Sepuluh09');
 
 --
 -- Indexes for dumped tables
@@ -54,6 +78,12 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +91,12 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
